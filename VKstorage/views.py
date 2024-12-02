f4221e7784baf8d6c870fd294be59607583f1206
Maxim Dietrich <dietrich.maxim@gmail.com>
2024-12-02 14:56:10 +0400

    v 0.6.3 - правки интерфейса

--- VKstorage/views.py
+++ VKstorage/views.py
@@ -4,13 +4,15 @@ from rest_framework.viewsets import ModelViewSet
 from VKstorage.models import Groups, Posts
 from VKstorage.serializers import AactivityInGroupsSerializer, PostsSerializer, GroupsSerializer
 
+from VKstorage.models import Groups
+from VKstorage.serializers import AactivityInGroupsSerializer
 
 @login_required
 def index(request):
     return render(request, 'VKstorage/index.html', {'title': 'Формирование отчета'})# Create your views here.
 @login_required
 def all_groups(request):
-    return render(request, 'VKstorage/groups.html', {'title': 'Формирование отчета'})# Create your views here.
+    return render(request, 'VKstorage/groups.html', {'title': 'Управление пабликами'})# Create your views here.
 
 class AactivityInGroupsView(ModelViewSet):
     queryset = Groups.objects.all()
